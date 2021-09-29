57c9ff3a26c6c870991bbca879feabac16cc39ba
libertus-codelin <91585349+libertus-codelin@users.noreply.github.com>
2021-09-29 13:28:05 +0700

    Menambahkan file php tentang tipe data

---
+++ tipedata.php
@@ -0,0 +1,25 @@
+<?php 
+   // 1) tipe data string
+   $string1 = 'Halo saya sedang belajar php di refactory';
+   echo $string1;
+   echo "<br/>";
+   $string2 = "Halo nama saya libertus";
+   echo $string2."<br/>";
+
+   // 2) tipe data integer
+   $angka1 = 15;
+   $angka2 = 10;
+   $hasil = ($angka1 + $angka2);
+   echo $hasil."<br/>";
+
+   // 3) tipe data float
+   $hargaJual = 7.500;
+   $hargaBeli = 8000;
+   echo "Harga jual barang adalah : Rp.".$hargaJual."<br/>";
+
+   // 4) tipe data boolean
+   $benar = true;
+   $salah = false;
+   echo "Benar = ".$benar." dan salah = ".$salah;
+
+?>
